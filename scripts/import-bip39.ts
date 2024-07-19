2c6a2371d590b0b3e2ccbacad64c4d12efb9a524
jk <47693+sectore@users.noreply.github.com>
2024-07-19 21:39:45 +0200

    fix import of slip39

--- scripts/import-bip39.ts
+++ scripts/import-bip39.ts
@@ -3,7 +3,9 @@ import { BunRuntime } from '@effect/platform-bun';
 import * as NodeFileSystem from '@effect/platform-node-shared/NodeFileSystem';
 import { Config, program } from './import-wordlist';
 import { GITHUB_URLS, RAW_GITHUB_URLS } from '../src/lib/const';
-import { languages } from '../src/lib/types';
+import { languages, WordListType } from '../src/lib/types';
+
+const type: WordListType = 'bip39';
 
 const all = pipe(
 	languages,
@@ -12,10 +14,10 @@ const all = pipe(
 			Config,
 			Config.of({
 				get: Effect.succeed({
-					type: 'bip39',
+					type,
 					lang,
-					github_raw: new URL(RAW_GITHUB_URLS.bip39[lang]),
-					github_url: new URL(GITHUB_URLS.bip39[lang]),
+          github_raw: new URL(RAW_GITHUB_URLS[type][lang]),
+          github_url: new URL(GITHUB_URLS[type][lang]),
 					pathToSave: `./src/lib/wordlists/bip39-${lang}.ts`
 				})
 			})
--- scripts/import-slip39.ts
+++ scripts/import-slip39.ts
@@ -3,7 +3,9 @@ import { BunRuntime } from '@effect/platform-bun';
 import * as NodeFileSystem from '@effect/platform-node-shared/NodeFileSystem';
 import { Config, program } from './import-wordlist';
 import { GITHUB_URLS, RAW_GITHUB_URLS } from '../src/lib/const';
+import { WordListType } from '../src/lib/types';
 
+const type: WordListType = 'slip39';
 // EN available only
 const lang = 'en';
 
@@ -11,10 +13,10 @@ const ConfigLayer = Layer.succeed(
 	Config,
 	Config.of({
 		get: Effect.succeed({
-			type: 'slip39',
+			type,
 			lang,
-			github_raw: new URL(RAW_GITHUB_URLS.bip39[lang]),
-			github_url: new URL(GITHUB_URLS.bip39[lang]),
+			github_raw: new URL(RAW_GITHUB_URLS[type][lang]),
+			github_url: new URL(GITHUB_URLS[type][lang]),
 			pathToSave: `./src/lib/wordlists/slip39-${lang}.ts`
 		})
 	})
