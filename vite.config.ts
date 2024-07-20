b10cf1bea5bd64f18f0fc268c99e1638a3974f45
jk <47693+sectore@users.noreply.github.com>
2024-07-20 09:36:15 +0200

    add vitest.config
    and merge options of vite.config
    
    See https://vitest.dev/config/file.html#managing-vitest-config-file
    
    Note: `test` option in vite.conifig does not work anymore for any reason

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,test.svelte,spec}.{js,ts}'],
-		environment: 'jsdom'
-	}
+	plugins: [sveltekit()]
 });
