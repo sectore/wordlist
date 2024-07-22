6b33cd898cc13753da1a3455048a957beb9db21f
jk <47693+sectore@users.noreply.github.com>
2024-07-22 12:28:47 +0200

    ExternalLink

--- src/routes/[slug]/list/+page.svelte
+++ src/routes/[slug]/list/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { GITHUB_URLS } from '$lib/const';
 	import NoResult from '$lib/NoResult.svelte';
 	import Search from '$lib/Search.svelte';
 	import store from '$lib/store.svelte';
+	import { ExternalLink } from 'lucide-svelte';
 </script>
 
 <div class="flex flex-grow flex-col">
@@ -10,10 +12,16 @@
 	<ul class="">
 		{#each store.wordlistFiltered as { pos, word }}
 			<li
-				class="flex flex-row items-end justify-center gap-4 border-t-2 border-dashed px-6 py-4 text-2xl text-gray-600 last:border-b-2 hover:bg-gray-50 dark:border-gray-600 dark:text-gray-300 dark:hover:bg-gray-800 md:text-4xl"
+				class="flex flex-row items-center justify-center gap-4 border-t-2 border-dashed px-6 py-4 text-2xl text-gray-600 last:border-b-2 hover:bg-gray-50 dark:border-gray-600 dark:text-gray-300 dark:hover:bg-gray-800 md:text-4xl"
 			>
-				<span class="text-xl text-gray-400 dark:text-gray-500 md:text-3xl">#{pos}</span>
-				{word}
+				<span class="text-gray-400 dark:text-gray-500">#{pos}</span>
+				<p class="flex-1 text-center">{word}</p>
+				<a
+					href={`${GITHUB_URLS[store.wordlistType][store.selectedLang]}#L${pos}`}
+					class="text-gray-400 hover:text-gray-600 dark:text-gray-500 hover:dark:text-gray-300"
+				>
+					<ExternalLink class="h-6 w-6 text-inherit md:h-8 md:w-8" />
+				</a>
 			</li>
 		{/each}
 	</ul>
