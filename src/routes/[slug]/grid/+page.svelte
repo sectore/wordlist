c24347d8c3180f797ca3d0a7ceea3ca34f1883f8
jk <47693+sectore@users.noreply.github.com>
2024-07-22 12:31:15 +0200

    link words to original source (#4)
    and fix order of languages

--- src/routes/[slug]/grid/+page.svelte
+++ src/routes/[slug]/grid/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { GITHUB_URLS } from '$lib/const';
 	import NoResult from '$lib/NoResult.svelte';
 	import Search from '$lib/Search.svelte';
 	import store from '$lib/store.svelte';
+	import { ExternalLink } from 'lucide-svelte';
 </script>
 
 <div class="flex flex-grow flex-col">
@@ -13,7 +15,15 @@
 				class="card border border-gray-100 shadow-md hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-900 dark:hover:bg-gray-800"
 			>
 				<div class="card-body">
-					<p class="text-lg text-gray-400 dark:text-gray-400 md:text-xl">#{pos}</p>
+					<div class="flex items-center text-gray-400 dark:text-gray-500">
+						<p class="flex-1 text-xl md:text-2xl">#{pos}</p>
+						<a
+							href={`${GITHUB_URLS[store.wordlistType][store.selectedLang]}#L${pos}`}
+							class="hover:text-gray-600 hover:dark:text-gray-300"
+						>
+							<ExternalLink class="h-6 w-6 text-inherit md:h-7 md:w-7" />
+						</a>
+					</div>
 					<h2 class="text-2xl text-gray-600 dark:text-gray-300 md:text-4xl">{word}</h2>
 				</div>
 			</div>
