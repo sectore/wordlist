97e45fc9d5fc2a524ca205ab502f6903c767af45
jk <47693+sectore@users.noreply.github.com>
2025-02-22 18:19:45 +0100

    fix: `TypeError: void 0 is not a function`
    in browser or `TypeError: Client.makeDefault is not a function` by
    running `bun test`.
    
    `@effect/platform-browser` needs to have latest version.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,6 @@
 	// To continue with `WordListType` an user used before
 	// re-direct to it
 	$effect(() => {
-		console.log('Effect redirect');
 		goto(`/wordlist/${store.wordlistType}`);
 	});
 </script>
